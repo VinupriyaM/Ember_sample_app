5bb96470ea6d54141556f5a5fbc654476600c82c
M <581689@cognizant.com>
2017-06-06 13:02:17 +0530

    Images and css changes

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -2,62 +2,46 @@
 import Ember from 'ember';
 let rentalhome = [{
     id: 'sample_1',
-    title: 'BuildingOne',
-    owner: 'Adam',
-    city: 'San Francisco',
-    description : 'one',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+    title: 'Christ Statue',
+    place: 'Brazil',
+    image: '../images/img2.jpg'
 
   }, {
     id: 'sample_2',
-    title: 'BuildingTwo',
-    owner: 'Bob',
-    city: 'Seattle',
-    description : 'two',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
+    title: 'Great Wall Of China',
+    place: 'China',
+    image: '../images/img4.jpg'
   }, {
     id: 'sample_3',
-    title: 'BuildingThree',
-    owner: 'Charlie',
-    city: 'Portland',
-    description : 'three',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
+    title: 'The Colosseum',
+    place: 'Italy',
+    image: '../images/img3.jpg'
   },{
       id: 'sample_4',
-      title: 'BuildingFour',
-      owner: 'David',
-      city: 'Finland',
-      description : 'four',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+      title: 'Macchu Picchu',
+      place: 'Peru',
+      image: '../images/img6.jpg'
     }, {
       id: 'sample_5',
-      title: 'BuildingFive',
-      owner: 'Emica',
-      city: 'Holland',
-      description : 'five',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
+      title: 'Chichen Itza',
+      place: 'Mexico',
+      image: '../images/img5.jpg'
     }, {
       id: 'sample_6',
-      title: 'BuildingSix',
-      owner: 'Frandis',
-      city: 'Germany',
-      description : 'six',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
+      title: 'Taj Mahal',
+      place: 'India',
+      image: '../images/img9.jpg'
     },
     {
       id: 'sample_7',
-      title: 'BuildingSeven',
-      owner: 'George',
-      city: 'Mexico',
-      description : 'seven',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+      title: 'Petra',
+      place: 'Jordan',
+      image: '../images/img7.jpg'
     },
     {
       id: 'sample_8',
-      title: 'BuildingEight',
-      owner: 'Henry',
-      city: 'France',
-      description : 'eight',
+      title: 'Kheops-Pyramid',
+      place: 'Egypt',
       image:'../images/img1.jpg'
 }];
 export default Ember.Route.extend({
